fb1667263696ada0c84b8fcb8eca14e008f253ec
anjasganteng <102732642+anjasganteng@users.noreply.github.com>
2022-04-01 18:33:47 +0700

    Update grupbot.js
    Az

--- result/lib/grupbot.js
+++ result/lib/grupbot.js
@@ -1,12 +1,12 @@
 const grupbot = (prefix) => { 
 	return `• *Grup Bot*
-*Link grup 1* : http://bit.ly/3i7Hgx9
-*Link grup 2* : http://bit.ly/3ibzCSs
-*Link grup 3* : http://bit.ly/36l3dpV
-*Link grup coding bersama* : http://bit.ly/36kz1em 
-*Link grup tes bot* : http://bit.ly/3ib0p12 
-*Link grup murid bot* : http://bit.ly/3IbZB6z
-*Link grup saling subscribe* : http://bit.ly/3Jh3FEb
+*Link grup 1* : http://bit.ly/
+*Link grup 2* : http://bit.ly/
+*Link grup 3* : http://bit.ly/
+*Link grup coding bersama* : http://bit.ly/
+*Link grup tes bot* : http://bit.ly/
+*Link grup murid bot* : http://bit.ly/
+*Link grup saling subscribe* : http://bit.ly/
 `
 }
 
